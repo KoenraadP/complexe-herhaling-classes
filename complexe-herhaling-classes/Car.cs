fb42197f8ca9d1ca6996be03add6a5fd5b93f343
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-02-18 12:01:25 +0100

    End of class 18/02

---
+++ complexe-herhaling-classes/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complexe_herhaling_classes
+{
+    internal class Car
+    {
+        // properties / eigenschappen van een auto
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool IsElectric { get; set; }
+        public decimal Price { get; set; }
+        public int AmountOfDoors { get; set; }
+        public Color Color { get; set; }
+
+        // een constructor maken om te zorgen
+        // dat bepaalde eigenschappen MOETEN ingevuld
+        // worden bij het aanmaken van een auto
+        // parameters met KLEINE letter starten
+        public Car(string brand, string model, 
+            int year)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+        }
+
+        // constructor zonder parameters
+        // dit laat dan toch nog toe 
+        // om een auto aan te maken zonder ingevulde properties
+        public Car()
+        {
+            
+        }
+
+        // override voor ToString zorgt dat bij
+        // het oproepen van de 'volledige' car in string formaat
+        // deze informatie getoond wordt
+        public override string ToString()
+        {
+            return Brand + " " + Model;
+        }
+    }
+}
--- complexe-herhaling-classes/Program.cs
+++ complexe-herhaling-classes/Program.cs
@@ -10,6 +10,27 @@ namespace complexe_herhaling_classes
     {
         static void Main(string[] args)
         {
+            // auto aanmaken op basis van class
+            Car car1 = new Car
+            {
+                Model = "M3",
+            };
+
+            // eigenschap Brand invullen
+            car1.Brand = "BMW";
+
+            // tweede auto zonder ingevulde properties
+            Car car2 = new Car();
+
+            // derde auto maken met constructor
+            Car car3 = new Car("Audi", "A4", 2019);
+
+            // waarde van Brand tonen
+            Console.WriteLine(car1.Brand);
+            // waarde van Model tonen
+            Console.WriteLine(car1.Model);
+            // merk en model van auto tonen door gewoon het car object op te vragen
+            Console.WriteLine(car1);
         }
     }
 }
